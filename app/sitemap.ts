e5dd66d674c16b177bca5acf5b93d11a950ac97a
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-10-21 09:07:11 -0500

    fix base url for sitemap and rss

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from "app/lib/utils"
 
-export const baseUrl = "https://portfolio-blog-starter.vercel.app"
+export const baseUrl = "https://www.jrar.dev"
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
